050fe84829c42b7182a09f98569d5a11b37f27f9
신동석 <96603945+dss1222@users.noreply.github.com>
2022-06-01 00:39:09 +0900

    리액트 추가 공부

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            
+            <div className="main-bg"></div>
+        </div>
+    );
 }
 
 export default App;
