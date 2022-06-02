3ca6e7eee378af5f52dec7c2b239a24863cb1a1f
신동석 <96603945+dss1222@users.noreply.github.com>
2022-06-02 11:30:37 +0900

    리액트 추가 공부

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import {useState} from "react";
 import logo from './logo.svg';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
-
+import data from './data.js';
 
 function App() {
+
+    let [shoes] = useState(data);
+
     return (
         <div className="App">
             <Navbar bg="light" variant="light">
@@ -15,10 +19,34 @@ function App() {
                     </Nav>
                 </Container>
             </Navbar>
-            
+
             <div className="main-bg"></div>
+
+            <div className="container">
+                <div className="row">
+                    {
+                        shoes.map((a, i)=>{
+                            return(
+                                <Card shoes={shoes[i]} i = {i}></Card>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
         </div>
     );
+}
+
+function Card(props) {
+    return(
+        <div className="col-md-4">
+            <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) +'.jpg'} width="80%"/>
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.content}</p>
+            <p>{props.shoes.price}</p>
+        </div>
+    )
 }
 
 export default App;
