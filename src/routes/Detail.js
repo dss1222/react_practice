d51042b98a6667419329d8a978de2fbbfff1a303
신동석 <96603945+dss1222@users.noreply.github.com>
2022-06-08 12:05:27 +0900

    리액트 추가 공부

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -17,8 +17,10 @@ function Detail(props) {
                 </div>
                 <div className="col-md-6 mt-4">
                     <h4 className="pt-5">{찾은상품.title}</h4>
+
                     <p>{찾은상품.content}</p>
                     <p>{찾은상품.price}원</p>
+
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
