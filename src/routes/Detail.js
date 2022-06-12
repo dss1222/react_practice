fe6dff228fa6093d963d423815a664922fd8a827
신동석 <96603945+dss1222@users.noreply.github.com>
2022-06-12 20:30:26 +0900

    리액트 추가 공부

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,6 +5,14 @@ import {useState, useEffect} from 'react';
 
 function Detail(props) {
 
+    useEffect(()=>{
+        console.log('안녕')
+    })
+
+    setTimeout(()=>{}, 2000)
+
+    let[count, setCount] = useState(0)
+
     let {id} = useParams();
     let 찾은상품 = props.shoes.find(function(x){
         return x.id == id
@@ -12,6 +20,9 @@ function Detail(props) {
 
     return (
         <div className="container">
+            <div className="alert alert-warning">2초이내 구매시 할인</div>
+            {count}
+            <button onClick={()=>{setCount(count+1) }}>버튼</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
